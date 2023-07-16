3d2dc1e18e08126c4ecbd4de4d03cd871c0970ea
Rudra1306 <rudraparikh6@gmail.com>
2023-07-16 19:14:04 +1000

    Initial Commit

---
+++ loginapp_auth/loginapp_authApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginapp_authApp.swift
+//  loginapp_auth
+//
+//  Created by Rudra on 16/07/23.
+//
+
+import SwiftUI
+
+@main
+struct loginapp_authApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
